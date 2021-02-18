c6324c90409b3400b987594e0f1e53b3db720ac5
Zzdamon <72194375+Zzdamon@users.noreply.github.com>
2021-02-18 17:38:38 +0200

    made logo

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -2,16 +2,35 @@ import React from 'react'
 import ProductItem from '../components/ProductItem';
 import { connect } from 'react-redux';
 import Layout from '../components/Layout';
+import { Link } from 'react-router-dom';
+
 
 function Favourites(props) {
         
     return (
-        <Layout>{
-    props.favourites.map(product=>
-    <ProductItem
-    {...product}
-    key={product.id}/>)
-}        
+
+        <Layout>
+            <div className="fav-page container-fluid container-min-max-width
+                d-flex flex-column justify-content-center align-items-center">
+            
+            {props.favourites.length>0
+            ?<div>
+                <h3>Produsele tale favorite:</h3>
+                <div className="container-fluid d-flex flex-wrap" >
+            {
+             props.favourites.map(product=>
+                <ProductItem
+                {...product}
+                key={product.id}/>)
+            }
+                </div>
+            </div>
+            :<div className="d-flex flex-column align-items-center">
+                        <p className="h3">Nu ai produse favorite!</p>
+                        <Link to="/"><button className="btn btn-outline-dark">Inapoi la home</button></Link>
+            </div>
+}
+</div>
 </Layout>
 
     )
