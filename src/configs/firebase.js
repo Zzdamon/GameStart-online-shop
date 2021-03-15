677b5a5c8371ac53a0bd7519826233097c5b4f4a
Lepirda Damon <72194375+Zzdamon@users.noreply.github.com>
2021-03-15 16:18:30 +0200

    Create firebase.js

---
+++ src/configs/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYBCOOsIr6fvoHiLq32sjSnz39oU30hEc",
+    authDomain: "online-shop-60b5c.firebaseapp.com",
+    projectId: "online-shop-60b5c",
+    storageBucket: "online-shop-60b5c.appspot.com",
+    messagingSenderId: "1011495862809",
+    appId: "1:1011495862809:web:3d0e1bf1983ef7087be088"
+  };
+  
+  export default firebaseConfig;
